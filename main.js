ae889c1b94ff01c98a262f8009c5e5a2a788dbb9
Dmitriy Stanev <90133347+dumoncter@users.noreply.github.com>
2023-08-14 22:40:13 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
-let tg = window.Telegram.WebApp;
+const tg = window.Telegram.WebApp;
 tg.expand();
 
+
 const onSubmit = (e) => {
-    document.getElementById("error").innerText = '';
     e.preventDefault();
     console.log('Hellooo');
-    const zywall = document.getElementById("zywall_ip").value;
-    const name = document.getElementById("name").value;
-    const mac = document.getElementById("mac").value;
-    // const error = document.getElementById("error");
-    // const errorMsgs = [];
+    const zywall = document.getElementById("zywall_ip").value,
+          mac = document.getElementById("mac").value,
+          mac_full = /^(([A-Fa-f0-9]{2}[:]){5}[A-Fa-f0-9]{2}[,]?)+$/i,
+          mac_2 = /^(([A-Fa-f0-9]{2}[:]){1}[A-Fa-f0-9]{2}[,]?)+$/i,
+          error_ip = document.getElementById("error_ip"),
+          error_mac = document.getElementById("error_mac"),
+          block_1 = document.getElementById("form"),
+          block_2 = document.getElementById("zabbix.add"),
+          select_ip = document.getElementById("zywall_ip"),
+          select_mac = document.getElementById("mac");
 
-    if (zywall.length < 10) {
-        document.getElementById("error").innerText = 'Ошибка. Вы не выбрали Zywall';
+    error_ip.innerText = "";
+    error_mac.innerText = "";
+    if (zywall === "") {
+        error_ip.innerText = "Выберите ip";
+        select_ip.style.border = "1px solid red";
         return;
     }
-    if (name.length >= 5) {
-        document.getElementById("error").innerText = 'Ошибка в имени';
+    if (mac_full.test(mac) || mac_2.test(mac)) {
+        console.log("Valid: " + mac);
+    } else {
+        console.log("InValid: " + mac);
+        select_mac.style.border = "1px solid red";
+        error_mac.innerText = 'Не верный Mac адрес! Введите последние 4 цифры или полный адрес';
         return;
     }
-    if (mac.length >= 5) {
-        document.getElementById("error").innerText = 'Ошибка mac';
-        return;
-    }
-
+    block_1.style.display = "none";
+    block_2.style.display = "flex";
 }
